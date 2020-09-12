f5e80735bedc4b6cfab4af5f2516ec24f0c9dee2
fings <1059372332@qq.com>
2020-09-12 09:14:29 +0800

    update L3

---
+++ main.js
@@ -0,0 +1,25 @@
+import {createElement, Component, render} from "./toy-react.js"
+
+class MyComponent extends Component {
+	constructor(){
+		super();
+		this.state = {
+			a: 1,
+			b: 2
+		}
+	}
+	render(){
+		return(
+			<div>
+				<h1>my component</h1>
+				<button onclick={()=>{ this.setState({a: this.state.a + 1})}}>add</button>
+				<div>{this.state.a.toString()}</div>
+				<div>{this.state.b.toString()}</div>
+			</div>
+		);
+	}
+}
+
+let component = <MyComponent/>;
+
+console.log(component.vdom);
